5e20dd3ebe9fd4531bd6b156ec9b23525505a872
BrightEghan <102719846+BrightEghan@users.noreply.github.com>
2023-02-06 06:45:21 -0800

    my first python code
    This ode displays a count timer, calendar and a simply calculator

---
+++ main.py
@@ -0,0 +1,48 @@
+# rows = int(input("How many rows?: "))
+# columns = int(input("How many columns?: "))
+# symbol = input(" Enter symbol to   be used?: ")
+# for i in range (rows):
+#     for j in range(columns):
+#       print(symbol,end ="")
+#     print()
+# import time
+# for seconds in range(10,0,-2):
+#     time.sleep(2)
+#     print("happy new year")
+# name = ""
+# while len(name)==0:
+#   name = input("Enter your name: ")
+#   print(name)
+import calendar
+year =int(input("enter year: "))
+mn =int(input("enter month: "))
+day =int(input("enter day :"))
+print(calendar.month(year,mn,day))
+# def add(x,y):
+#   return(x+y)
+# def subtract(x,y):
+#     return(x-y)
+# def divide(x,y):
+#       return(x/y)
+# def multiply(x,y):
+#         return(x*y)
+# print("select operation. ")
+# print("1.add")
+# print("2.subtract")
+# print("3.divide")
+# print("4.multiply")
+# choice = input("enter choice(1/2/3/4): ")
+# num1 = int(input("enter first number: "))
+# num2 = int(input("enrer  second number: "))
+# if choice =="1":
+#           print(num1,"+",num2,"=",add(num1,num2))
+# elif choice =="2":
+#           print(num1,"-",num2,"=",subtract(num1,num2))
+# elif choice =="3":
+#           print(num1,"/",num2, "=",divide(num1,num2))
+# elif choice =="4":
+#           print(num1,"*",num2, "=",multiply(num1,num2))
+# else :
+#           print("Invalid input")
+
+
